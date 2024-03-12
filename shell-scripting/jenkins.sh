f28e4a9b2ffbc0a5e8491a0bb2fbb1f62aad4d54
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-12 16:19:47 +0530

    update code

---
+++ shell-scripting/jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install git maven tree -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install java-17
+sudo yum install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
