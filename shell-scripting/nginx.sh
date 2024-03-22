a2a53c31c130d5b52e48c1ca663bbd53f27133c1
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-22 13:06:13 +0530

    update

--- shell-scripting/nginx.sh
+++ shell-scripting/nginx.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-sudo yum update -y
+#!/ bin/bash
 sudo yum install nginx -y
 sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl enbale nginx
+sudo systemctl status nginx
+cd /opt/
+sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page292/microo.zip
+sudo unzip  microo.zip
+sudo rm -rf  microo.zip
+sudo rm -rf /usr/share/nginx/html/*
+sudo cp -r /opt/microo-html/* /usr/share/nginx/html/
+sudo rm -rf /opt/microo-html
--- shell-scripting/tomcat.sh
+++ shell-scripting/tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install java -y
-wget http://dlcdc.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
-sudo tar -zxvf apache-tomcat-9.0.80.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87.zip
+sudo unzip apache-tomcat-9.0.87.zip
